ab99cfa503c2fcddb44246f8311574a2f5ec64d7
Arunprasathr666 <arunprasathr666@gmail.com>
2025-02-05 18:55:09 +0530

    function changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import { PasswordGenerator } from './passwordGenerator'
+import { PasswordGenerator } from "./PasswordGenerator";
+
 
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
 
   return (
     <>
-      <PasswordGenerator/>
-    </>
+    <PasswordGenerator/>
+      </>
+      
   )
 }
 
